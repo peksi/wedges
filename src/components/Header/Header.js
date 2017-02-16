fe39535bea14ccd7861958c839e7321b3d7bd250
Pekka Lammi <pekka.lammi@aalto.fi>
2017-02-16 11:41:25 +0200

    do not text-center everything

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { IndexLink, Link } from 'react-router'
 import './Header.scss'
 
 export const Header = () => (
-  <div>
+  <div className='header'>
     <h1>Wedges game</h1>
     <IndexLink to='/' activeClassName='route--active'>
       Home
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 import './HomeView.scss'
 
 export const HomeView = () => (
-  <div>
+  <div className='homeview'>
     <h4>Welcome!</h4>
     <p>This is a place for basic survey and to tell the motivation of this survey.</p>
     <Link to='/reduce' activeClassName='route--active'>
