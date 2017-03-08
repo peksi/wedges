d3f293ebad5153a0b1aeab769da0b987218c3a0d
Pekka Lammi <pekka.lammi@aalto.fi>
2017-03-08 12:42:29 +0200

    add description to config

--- config/wedges.config.js
+++ config/wedges.config.js
@@ -1,19 +1,64 @@
 module.exports = {
   choices: [
-    'Transport Efficiency',
-    'Transport Conservation',
-    'Builiding Efficiency',
-    'Efficiency in Electiricty Production',
-    'CSS Electricity',
-    'CSS Hydrogen',
-    'CSS Synfuels',
-    'Fuel-Switching for Electricity',
-    'Nuclear Electricity',
-    'Wind Electricity',
-    'Solar Electricity',
-    'Wind Hydrogen',
-    'Biofuels',
-    'Forest Storage',
-    'Soil Storage'
+    {
+      name: 'Transport Efficiency',
+      description: 'Increase automobile fuel efficiency (2 billion cars projected in 2050)'
+    },
+    {
+      name: 'Transport Conservation',
+      description: 'Reduce miles traveled by passenger and/or freight vehicles'
+    },
+    {
+      name: 'Builiding Efficiency',
+      description: 'Increase insulation, furnace and lighting efficiency'
+    },
+    {
+      name: 'Efficiency in Electiricty Production',
+      description: 'Increase efficiency of power generation '
+    },
+    {
+      name: 'CSS Electricity',
+      description: '90% of CO2 from fossil fuel power plants captured, then stored underground (800 large coal plants or 1600 natural gas plants)'
+    },
+    {
+      name: 'CSS Hydrogen',
+      description: 'Hydrogen fuel from fossil sources with CCS displaces hydrocarbon fuels '
+    },
+    {
+      name: 'CSS Synfuels',
+      description: 'Capture and store CO2 emitted during synfuels production from coal'
+    },
+    {
+      name: 'Fuel-Switching for Electricity',
+      description: 'Replacing coal-burning electric plants with natural gas plants (1400 1 GW coal plants)'
+    },
+    {
+      name: 'Nuclear Electricity',
+      description: 'Displace coal-burning electric plants with nuclear plants (Add double current capacity)'
+    },
+    {
+      name: 'Wind Electricity',
+      description: 'Wind displaces coal-based electricity (10 x current capacity)'
+    },
+    {
+      name: 'Solar Electricity',
+      description: 'Solar PV displaces coal-based electricity (100 x current capacity)'
+    },
+    {
+      name: 'Wind Hydrogen',
+      description: 'Produce hydrogen with wind electricity'
+    },
+    {
+      name: 'Biofuels',
+      description: 'Biomass fuels from plantations replace petroleum fuels'
+    },
+    {
+      name: 'Forest Storage',
+      description: 'Carbon stored in new forests'
+    },
+    {
+      name: 'Soil Storage',
+      description: 'Farming techniques increase carbon retention or storage in soils'
+    }
   ]
 }
