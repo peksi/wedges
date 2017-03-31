980be1b25c65ed73e9931f7817d98de0bbb275aa
Pekka Lammi <pekka.lammi@aalto.fi>
2017-03-31 15:08:09 +0300

    remove double descriptions

--- src/routes/Reduce/components/ReduceView.js
+++ src/routes/Reduce/components/ReduceView.js
@@ -3,16 +3,14 @@ import BatchViewContainer from './BatchView'
 
 export const ReduceView = () => (
   <div className='row'>
-  <div className='col-sm-6'>
-    <h3>Create a portfolio of 8 wedges</h3>
-    <p>Remove wedges by clicking them.</p>
-    <BatchViewContainer direction={'reduce'} />
-  </div>
-  <div className='col-sm-6'>
-    <h3>Removed wedges</h3>
-    <p>These are the wedges that you have already selected out. You can put restore wedges by clicking them.</p>
-    <BatchViewContainer direction={'return'} />
-  </div>
+    <div className='col-sm-6'>
+      <h3>Strategies that are not selected</h3>
+      <BatchViewContainer direction={'reduce'} />
+    </div>
+    <div className='col-sm-6'>
+      <h3>Your choices</h3>
+      <BatchViewContainer direction={'return'} />
+    </div>
   </div>
 )
 
