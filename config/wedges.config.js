d47a20d321c5d059fe9f0a81367ed36b012a914e
Pekka Lammi <pekka.lammi@aalto.fi>
2017-03-31 15:08:30 +0300

    index wedges

--- config/wedges.config.js
+++ config/wedges.config.js
@@ -1,62 +1,77 @@
 module.exports = {
   choices: [
     {
+      id: 1,
       name: 'Transport Efficiency',
       description: 'Increase automobile fuel efficiency (2 billion cars projected in 2050)'
     },
     {
+      id: 2,
       name: 'Transport Conservation',
       description: 'Reduce miles traveled by passenger and/or freight vehicles'
     },
     {
+      id: 3,
       name: 'Builiding Efficiency',
       description: 'Increase insulation, furnace and lighting efficiency'
     },
     {
+      id: 4,
       name: 'Efficiency in Electiricty Production',
       description: 'Increase efficiency of power generation '
     },
     {
+      id: 5,
       name: 'CSS Electricity',
       description: '90% of CO2 from fossil fuel power plants captured, then stored underground (800 large coal plants or 1600 natural gas plants)'
     },
     {
+      id: 6,
       name: 'CSS Hydrogen',
       description: 'Hydrogen fuel from fossil sources with CCS displaces hydrocarbon fuels '
     },
     {
+      id: 7,
       name: 'CSS Synfuels',
       description: 'Capture and store CO2 emitted during synfuels production from coal'
     },
     {
+      id: 8,
       name: 'Fuel-Switching for Electricity',
       description: 'Replacing coal-burning electric plants with natural gas plants (1400 1 GW coal plants)'
     },
     {
+      id: 9,
       name: 'Nuclear Electricity',
       description: 'Displace coal-burning electric plants with nuclear plants (Add double current capacity)'
     },
     {
+      id: 10,
       name: 'Wind Electricity',
       description: 'Wind displaces coal-based electricity (10 x current capacity)'
     },
     {
+      id: 11,
       name: 'Solar Electricity',
       description: 'Solar PV displaces coal-based electricity (100 x current capacity)'
     },
     {
+      id: 12,
       name: 'Wind Hydrogen',
       description: 'Produce hydrogen with wind electricity'
     },
     {
+      id: 13,
       name: 'Biofuels',
       description: 'Biomass fuels from plantations replace petroleum fuels'
     },
     {
+      id: 14,
       name: 'Forest Storage',
       description: 'Carbon stored in new forests'
     },
     {
+      id: 15,
       name: 'Soil Storage',
       description: 'Farming techniques increase carbon retention or storage in soils'
     }
