041e20f45e20d2beaf79a749cae7bd384b392e48
Pekka Lammi <pekka.lammi@aalto.fi>
2017-04-04 17:25:08 +0300

    change buttons to bootstrap-buttons

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router'
 import './HomeView.scss'
+import { Button } from 'react-bootstrap'
 
 export const HomeView = () => (
   <div className='homeview'>
     <h4>Welcome!</h4>
     <p>This is a place for basic survey and to tell the motivation of this survey.</p>
     <Link to='/form' activeClassName='route--active'>
-      <button>Continue</button>
+      <Button>Continue</Button>
     </Link>
   </div>
 )
