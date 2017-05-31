3dcb130886867448d271001ab8aa40409e87da1c
Pekka Lammi <pekka.lammi@aalto.fi>
2017-05-31 11:54:04 +0300

    click-through homeview

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -8,43 +8,53 @@ import ppt3 from '../assets/ppt3.png'
 
 export const HomeView = (props) => (
   <div className='homeview'>
-    <div className='page1'>
-      <h4>Welcome to CarbCut</h4>
-      <p>
-        We are interested in how you would tackle the Climate Problem with existing stabilization technologies
-      </p>
-      <p>
-        This decision process will take about 20 minutes
-      </p>
-      <img src={ppt1} style={{ 'margin': 'auto', 'display':'block' }} />
-      <p className='reference'>This website is based on the “Stabilization Wedges” concept first presented in <br />
-        "Stabilization Wedges: Solving the Climate Problem for the next 50 Years with Current Technologies," <br />
-        S. Pacala and R. Socolow, Science, August 13, 2004. <br />
-        We acknowledge the Carbon Mitigation Initiative, Princeton University</p>
-      <Button className='next-button' onClick={() => { props.goForward() }}>Next</Button>
-    </div>
-    <div className='page2'>
-      <img src={ppt2} />
-      <Button className='next-button'>Next</Button>
-    </div>
-    <div className='page3'>
-      <img src={ppt3} />
-      <Button className='next-button'>Next</Button>
-    </div>
-    <div className='page4'>
-      <h3>When creating your basket</h3>
-      <p>You will see descriptions of the possible strategies with cost estimates and challenges</p>
-      <p>Consider different perspectives when deciding</p>
-      <ul>
-        <li>Environmental</li>
-        <li>Economic</li>
-        <li>Social</li>
-        <li>Political</li>
-      </ul>
-      <Link to='/reduce' className='next-button'>
-        <Button>Next</Button>
-      </Link>
-    </div>
+    {props.page === 0
+      ? <div className='page1' style={{ 'display': (props.page === 0) ? 'hidden' : 'visible' }}>
+        <h4>Welcome to CarbCut</h4>
+        <p>
+          We are interested in how you would tackle the Climate Problem with existing stabilization technologies
+        </p>
+        <p>
+          This decision process will take about 20 minutes
+        </p>
+        <img src={ppt1} style={{ 'margin': 'auto', 'display':'block' }} />
+        <p className='reference'>This website is based on the “Stabilization Wedges” concept first presented in <br />
+          "Stabilization Wedges: Solving the Climate Problem for the next 50 Years with Current Technologies," <br />
+          S. Pacala and R. Socolow, Science, August 13, 2004. <br />
+          We acknowledge the Carbon Mitigation Initiative, Princeton University</p>
+        <Button className='next-button' onClick={() => { props.goForward() }}>Next</Button>
+      </div>
+      : ''}
+    {props.page === 1
+      ? <div className='page2'>
+        <img src={ppt2} />
+        <Button className='next-button' onClick={() => { props.goForward() }}>Next</Button>
+      </div>
+      : ''}
+    {props.page === 2
+      ? <div className='page3'>
+        <img src={ppt3} />
+        <Button className='next-button' onClick={() => { props.goForward() }}>Next</Button>
+      </div>
+      : ''
+    }
+    {props.page === 3
+      ? <div className='page4'>
+        <h3>When creating your basket</h3>
+        <p>You will see descriptions of the possible strategies with cost estimates and challenges</p>
+        <p>Consider different perspectives when deciding</p>
+        <ul>
+          <li>Environmental</li>
+          <li>Economic</li>
+          <li>Social</li>
+          <li>Political</li>
+        </ul>
+        <Link to='/reduce' className='next-button'>
+          <Button>Next</Button>
+        </Link>
+      </div>
+      : ''
+    }
   </div>
 )
 
@@ -56,10 +66,12 @@ const mapDispatchToProps = {
 }
 
 const mapStateToProps = (state) => ({
+  page: state.reduce.page
 })
 
 HomeView.propTypes = {
-  goForward: React.PropTypes.func.isRequired
+  goForward: React.PropTypes.func.isRequired,
+  page: React.PropTypes.number
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeView)
