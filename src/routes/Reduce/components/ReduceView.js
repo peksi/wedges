00245cb7d88c61f86047ca0e6214157821445b0b
Pekka Lammi <pekka.lammi@aalto.fi>
2017-06-29 16:04:51 +0300

    improve reduceview

--- src/routes/Reduce/components/ReduceView.js
+++ src/routes/Reduce/components/ReduceView.js
@@ -9,31 +9,44 @@ import './ReduceView.scss'
 export const ReduceView = (props) => (
   <div className='row'>
     {props.basketHidden
-      ? <Button onClick={() => { console.log('show'); props.showBasket() }}> Next </Button>
+      ? <div className='helpbox'>
+        <p>Your decision task:
+          <ul>
+            <li>
+              Create a basket of 8 strategies based on your preferences.
+            </li>
+            <li>
+              Take into account the perspectives you find relevant, e.g. environmental, economic, social, political.
+            </li>
+          </ul>
+          In this task the <b>starting point</b> is that you have <b>too many strategies</b> in your basket.
+        </p>
+        <Button onClick={() => { console.log('show'); props.showBasket() }}> Next </Button>
+      </div>
        : ''}
     {(!props.basketHidden && props.reduceCount > 8)
       ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
           You have {props.reduceCount} strategies in your basket. Please remove {props.reduceCount - 8}.
       </div> : ''}
     {(props.reduceCount === 8)
-      ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
+      ? <div className='description text-center helpbox' style={{ fontSize: 22 + 'px' }}>
           You now have the required number of strategies in your basket.
           You can still make changes. <br /> If you are happy with your basket, press confirm.
           <Link to='/middlepage'>
-            <Button> Confirm </Button>
+            <Button style={{float: 'right'}} bsStyle='default'> Confirm </Button>
           </Link>
       </div> : ''}
     {(props.reduceCount < 8) ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
         There are {8 - props.reduceCount} strategies missing from your basket.
       </div> : ''}
-    <div className='col-sm-6'>
+    <div className='col-sm-6 basket'>
       <h3>Removed strategies</h3>
       <p>Press the add button to return a strategy into the basket</p>
       {props.basketHidden ? ''
       : <BatchViewContainer direction={'reduce'} />
       }
     </div>
-    <div className='col-sm-6'>
+    <div className='col-sm-6 basket'>
       <h3>Basket</h3>
       <p>Press the remove button to remove a strategy from the basket</p>
       {props.basketHidden ? ''
