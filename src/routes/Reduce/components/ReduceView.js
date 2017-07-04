faebd58c233c4e3f7a5f10aa242ad5635833e2d2
Pekka Lammi <pekka.lammi@aalto.fi>
2017-07-04 11:32:53 +0300

    animation in reduce

--- src/routes/Reduce/components/ReduceView.js
+++ src/routes/Reduce/components/ReduceView.js
@@ -6,10 +6,13 @@ import './ReduceView.scss'
 
 // import wedgesconfig from '../../../../config/wedges.config.js'
 
-export const ReduceView = (props) => (
-  <div className='row'>
-    {props.basketHidden
-      ? <div className='helpbox'>
+export const ReduceView = (props) => {
+  const componentClasses = ['helpbox']
+  if (!props.basketHidden) { componentClasses.push('hide-helpbox') }
+
+  return (
+    <div className='row'>
+      <div className={componentClasses.join(' ')}>
         <p>Your decision task:</p>
         <ul>
           <li>
@@ -24,38 +27,38 @@ export const ReduceView = (props) => (
         </p>
         <Button className='confirmHelp' onClick={() => { props.showBasket() }}> Next </Button>
       </div>
-       : ''}
-    {(!props.basketHidden && props.reduceCount > 8)
-      ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
-          You have {props.reduceCount} strategies in your basket. Please remove {props.reduceCount - 8}.
-      </div> : ''}
-    {(props.reduceCount === 8)
-      ? <div className='description text-center helpbox' style={{ fontSize: 22 + 'px' }}>
-          You now have the required number of strategies in your basket.
-          You can still make changes. <br /> If you are happy with your basket, press confirm.
-          <Link to='/middlepage'>
-            <Button style={{ float: 'right' }} bsStyle='default'> Confirm </Button>
-          </Link>
-      </div> : ''}
-    {(props.reduceCount < 8) ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
-        There are {8 - props.reduceCount} strategies missing from your basket.
-      </div> : ''}
-    <div className='col-sm-6 basket'>
-      <h3>Removed strategies</h3>
-      <p>Press the add button to return a strategy into the basket</p>
-      {props.basketHidden ? ''
-      : <BatchViewContainer direction={'reduce'} />
-      }
+      {(!props.basketHidden && props.reduceCount > 8)
+        ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
+            You have {props.reduceCount} strategies in your basket. Please remove {props.reduceCount - 8}.
+        </div> : ''}
+      {(props.reduceCount === 8)
+        ? <div className='description text-center helpbox' style={{ fontSize: 22 + 'px' }}>
+            You now have the required number of strategies in your basket.
+            You can still make changes. <br /> If you are happy with your basket, press confirm.
+            <Link to='/middlepage'>
+              <Button style={{ float: 'right' }} bsStyle='default'> Confirm </Button>
+            </Link>
+        </div> : ''}
+      {(props.reduceCount < 8) ? <div className='description text-center' style={{ fontSize: 22 + 'px' }}>
+          There are {8 - props.reduceCount} strategies missing from your basket.
+        </div> : ''}
+      <div className='col-sm-6 basket'>
+        <h3>Removed strategies</h3>
+        <p>Press the add button to return a strategy into the basket</p>
+        {props.basketHidden ? ''
+        : <BatchViewContainer direction={'reduce'} />
+        }
+      </div>
+      <div className='col-sm-6 basket'>
+        <h3>Basket</h3>
+        <p>Press the remove button to remove a strategy from the basket</p>
+        {props.basketHidden ? ''
+        : <BatchViewContainer direction={'return'} />
+        }
+      </div>
     </div>
-    <div className='col-sm-6 basket'>
-      <h3>Basket</h3>
-      <p>Press the remove button to remove a strategy from the basket</p>
-      {props.basketHidden ? ''
-      : <BatchViewContainer direction={'return'} />
-      }
-    </div>
-  </div>
-)
+  )
+}
 
 import { connect } from 'react-redux'
 import { showBasket } from '../modules/reduce'
