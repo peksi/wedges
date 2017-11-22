478f8b5550abb3eb6b01d486b0b7cb0cd71b11d2
Pekka Lammi <pekka.lammi@aalto.fi>
2017-11-22 12:42:50 +0200

    improved survey

--- src/routes/Survey/components/Survey.js
+++ src/routes/Survey/components/Survey.js
@@ -8,30 +8,53 @@ import { browserHistory } from 'react-router'
 
 class Survey extends React.Component {
   render () {
-    this.submitInfo = {
-      reducedValues: this.props.reduce.reducedValues,
-      reducedBasketLog: this.props.reduce.log,
-      addedValues: this.props.add.addedValues,
-      addedBasketLog: this.props.add.log,
-      thankyou: {
-        left: this.props.thankyou.first,
-        better: this.props.thankyou.better
-      },
-      values: this.props.state.form.lol ? this.props.state.form.lol.values : null
+    const gatherSubmitInfo = () => {
+      try {
+        return {
+          groupcode: this.props.state.groupcode.groupcode,
+          // result values
+          addedValues: this.props.add.addedValues,
+          reducedValues: this.props.reduce.reducedValues,
+          // which one was first
+          orderOfProcedures: this.props.state.home.first,
+          // locations of initial procedures
+          initialAddOrder: this.props.add.initialOrder,
+          initialReduceOrder: this.props.initialOrder,
+          // evaluation of baskets
+          evaluationOrder: this.props.thankyou.first,
+          // NB: evaluation grade comes from the survey!
+          // survey, if filled
+          // includes evaluation and personal data
+          survey: this.props.state.form.lol ? this.props.state.form.lol.values : null,
+          // log of phases
+          log: this.props.state.home.log,
+
+          // irrelevant for now
+          reducedBasketLog: this.props.reduce.log,
+          addedBasketLog: this.props.add.log,
+        }
+      } catch (e) {
+        console.log(e)
+        return {
+          'result': 'no data'
+        }
+      }
     }
+
+    this.submitInfo = gatherSubmitInfo()
 
     this.asd = () => {
       console.log(this.submitInfo)
+      var request = new XMLHttpRequest()
+      request.open('POST', '/api', true)
+      request.setRequestHeader('Content-Type', 'application/json')
+      request.send(JSON.stringify(this.submitInfo))
     }
     return (
       <div className='formview text-center'>
 
-        <p>You have now completed your decision task. Please anwer the following questions.</p>
-        <hr />
         <form
-          className='surveyForm'
           style={{
-            'width': 50 + '%',
             'display': 'block',
             'margin': 'auto',
             'textAlign': 'left'
@@ -43,28 +66,98 @@ class Survey extends React.Component {
           })}
         >
           <div className='page1' style={this.props.page === 1 ? { display: 'block' } : { display:'none' }}>
-            <div>
-              <label>When creating a basket of strategies, which procedure you felt more confident with?</label>
-              <div className='radio'>
-                <label>
-                  <Field name='strategy' component='input' type='radio' value='add' />
-                  Starting with an empty basket and adding strategies into it.
-                </label>
+            <h2>You have now completed your decision task. Please answer the following questions.</h2>
+            <hr />
+
+            <div className='radioboxsurvey'>
+              <div className='radioboxsurvey'>
+                <div>
+                  <h2>You created a basket of strategies following two procedures. Please rate procedures used.</h2>
+                  <label>Adding strategies into the basket</label>
+                  <div>
+                    <span className='fieldWrapper'><Field name='adding_strategies' component='input' type='radio' value='1' /> very easy </span>
+                    <span className='fieldWrapper'><Field name='adding_strategies' component='input' type='radio' value='2' /> easy </span>
+                    <span className='fieldWrapper'><Field name='adding_strategies' component='input' type='radio' value='3' /> not easy nor difficult </span>
+                    <span className='fieldWrapper'><Field name='adding_strategies' component='input' type='radio' value='4' /> difficult </span>
+                    <span className='fieldWrapper'><Field name='adding_strategies' component='input' type='radio' value='5' /> very difficult </span>
+                  </div>
+                  <label>Removing extra strategies from the basket</label>
+                  <div>
+                    <span className='fieldWrapper'><Field name='removing_strategies' component='input' type='radio' value='1' /> very easy </span>
+                    <span className='fieldWrapper'><Field name='removing_strategies' component='input' type='radio' value='2' /> easy </span>
+                    <span className='fieldWrapper'><Field name='removing_strategies' component='input' type='radio' value='3' /> not easy nor difficult </span>
+                    <span className='fieldWrapper'><Field name='removing_strategies' component='input' type='radio' value='4' /> difficult </span>
+                    <span className='fieldWrapper'><Field name='removing_strategies' component='input' type='radio' value='5' /> very difficult </span>
+                  </div>
+                </div>
               </div>
-              <div className='radio'>
-                <label>
-                  <Field name='strategy' component='input' type='radio' value='reduce' />
-                  Starting with with too many strategies in the basket and reducing strategies from it.
-                </label>
+              <hr />
+
+              <h2>Which perspectives did you consider when creating the baskets?</h2>
+              <div>
+                <label>Costs</label>
+                <div>
+                  <span className='fieldWrapper'><Field name='costs' component='input' type='radio' value='1' /> not at all </span>
+                  <span className='fieldWrapper'><Field name='costs' component='input' type='radio' value='2' /> somewhat </span>
+                  <span className='fieldWrapper'><Field name='costs' component='input' type='radio' value='3' /> moderately </span>
+                  <span className='fieldWrapper'><Field name='costs' component='input' type='radio' value='4' /> much </span>
+                  <span className='fieldWrapper'><Field name='costs' component='input' type='radio' value='5' /> very much </span>
+                </div>
               </div>
-              <div className='radio'>
-                <label>
-                  <Field name='strategy' component='input' type='radio' value='no_preference' />
-                  I don´t know / No answer
-                </label>
+              <div>
+                <label>Feasibility</label>
+                <div>
+                  <span className='fieldWrapper'><Field name='feasibility' component='input' type='radio' value='1' /> not at all </span>
+                  <span className='fieldWrapper'><Field name='feasibility' component='input' type='radio' value='2' /> somewhat </span>
+                  <span className='fieldWrapper'><Field name='feasibility' component='input' type='radio' value='3' /> moderately </span>
+                  <span className='fieldWrapper'><Field name='feasibility' component='input' type='radio' value='4' /> much </span>
+                  <span className='fieldWrapper'><Field name='feasibility' component='input' type='radio' value='5' /> very much </span>
+                </div>
+              </div>
+              <div>
+                <label>Sustainability</label>
+                <div>
+                  <span className='fieldWrapper'><Field name='sustainability' component='input' type='radio' value='1' /> not at all </span>
+                  <span className='fieldWrapper'><Field name='sustainability' component='input' type='radio' value='2' /> somewhat </span>
+                  <span className='fieldWrapper'><Field name='sustainability' component='input' type='radio' value='3' /> moderately </span>
+                  <span className='fieldWrapper'><Field name='sustainability' component='input' type='radio' value='4' /> much </span>
+                  <span className='fieldWrapper'><Field name='sustainability' component='input' type='radio' value='5' /> very much </span>
+                </div>
+              </div>
+              <div>
+                <label>Balance across categories</label>
+                <div>
+                  <span className='fieldWrapper'><Field name='balance' component='input' type='radio' value='1' /> not at all </span>
+                  <span className='fieldWrapper'><Field name='balance' component='input' type='radio' value='2' /> somewhat </span>
+                  <span className='fieldWrapper'><Field name='balance' component='input' type='radio' value='3' /> moderately </span>
+                  <span className='fieldWrapper'><Field name='balance' component='input' type='radio' value='4' /> much </span>
+                  <span className='fieldWrapper'><Field name='balance' component='input' type='radio' value='5' /> very much </span>
+                </div>
+              </div>
+              <div>
+                <label>Global fairness</label>
+                <div>
+                  <span className='fieldWrapper'><Field name='global_fairness' component='input' type='radio' value='1' /> not at all </span>
+                  <span className='fieldWrapper'><Field name='global_fairness' component='input' type='radio' value='2' /> somewhat </span>
+                  <span className='fieldWrapper'><Field name='global_fairness' component='input' type='radio' value='3' /> moderately </span>
+                  <span className='fieldWrapper'><Field name='global_fairness' component='input' type='radio' value='4' /> much </span>
+                  <span className='fieldWrapper'><Field name='global_fairness' component='input' type='radio' value='5' /> very much </span>
+                </div>
+              </div>
+              <div>
+                <label>Did you have another perspectives? Please list</label>
+                <div>
+                  <Field name='other' component='input' type='text' />
+                </div>
               </div>
             </div>
             <hr />
+            <div>
+              <label>General comments about the procedures</label>
+              <div>
+                <Field name='comments' component='input' type='text' />
+              </div>
+            </div>
 
             <Button
               style={{ 'float':'right' }}
@@ -74,30 +167,38 @@ class Survey extends React.Component {
             > Go forward </Button>
           </div>
           <div className='page2' style={this.props.page === 2 ? { display: 'block' } : { display:'none' }}>
+            <h2>Finally, please answer the following background questions.</h2>
+            <hr />
             <div>
-              <label>About you</label>
+              <label>I am</label>
               <div className='radio'>
                 <label>
-                  <Field name='aboutyou' component='input' type='radio' value='professional' />
-                  I´m a professional
+                  <Field name='aboutyou' component='input' type='radio' value='working' />
+                  Working
                 </label>
               </div>
               <div className='radio'>
                 <label>
-                  <Field name='aboutyou' component='input' type='radio' value='student' />
-                  I´m a student
+                  <Field name='aboutyou' component='input' type='radio' value='studying' />
+                  Studying
+                </label>
+              </div>
+              <div className='radio'>
+                <label>
+                  <Field name='aboutyou' component='input' type='radio' value='retired' />
+                  Retired
                 </label>
               </div>
               <div className='radio'>
                 <label>
                   <Field name='aboutyou' component='input' type='radio' value='no_answer' />
-                  No answer
+                  Other / No answer
                 </label>
               </div>
             </div>
 
             <div>
-              <label>If you are a student, select one</label>
+              <label>Highest degree earned</label>
               <div className='radio'>
                 <label>
                   <Field name='degree' component='input' type='radio' value='high_school' />
@@ -108,16 +209,16 @@ class Survey extends React.Component {
                 <label><Field name='degree' component='input' type='radio' value='bachelor' /> Bachelor </label>
               </div>
               <div className='radio'>
-                <label><Field name='degree' component='input' type='radio' value='master' /> Master </label>
+                <label><Field name='degree' component='input' type='radio' value='vocational' /> Vocational </label>
               </div>
               <div className='radio'>
-                <label><Field name='degree' component='input' type='radio' value='doctoral' /> Doctoral degree </label>
+                <label><Field name='degree' component='input' type='radio' value='master' /> Master´s </label>
               </div>
               <div className='radio'>
-                <label><Field name='degree' component='input' type='radio' value='no answer' /> Other</label>
+                <label><Field name='degree' component='input' type='radio' value='doctoral' /> Doctoral </label>
               </div>
               <div className='radio'>
-                <label><Field name='degree' component='input' type='radio' value='no answer' /> No answer </label>
+                <label><Field name='degree' component='input' type='radio' value='no answer' /> Other / No answer</label>
               </div>
             </div>
             <div>
@@ -153,7 +254,16 @@ class Survey extends React.Component {
                   component='input'
                   type='radio'
                   value='education' />
-                    Education, Health, Medical
+                    Education
+                </label>
+              </div>
+              <div className='radio'>
+                <label><Field
+                  name='occupation'
+                  component='input'
+                  type='radio'
+                  value='healthcare' />
+                    Healthcare
                 </label>
               </div>
               <div className='radio'>
@@ -186,10 +296,7 @@ class Survey extends React.Component {
                 </label>
               </div>
               <div className='radio'>
-                <label><Field name='occupation' component='input' type='radio' value='other' />Other</label>
-              </div>
-              <div className='radio'>
-                <label><Field name='occupation' component='input' type='radio' value='no_answer' />No answer</label>
+                <label><Field name='occupation' component='input' type='radio' value='other' />Other / No answer</label>
               </div>
             </div>
 
@@ -197,6 +304,13 @@ class Survey extends React.Component {
               <label>Your country</label>
               <div>
                 <NationalityDropdown />
+              </div>
+            </div>
+
+            <div>
+              <label>Age</label>
+              <div>
+                <Field component='input' type='number' name='age'></Field>
               </div>
             </div>
 
@@ -250,5 +364,6 @@ const mapStateToProps = (state) => ({
 Survey = connect(mapStateToProps, mapDispatchToProps)(Survey) //eslint-disable-line
 
 export default reduxForm({
-  form: 'lol'
+  form: 'lol',
+  destroyOnUnmount: false
 })(Survey)
